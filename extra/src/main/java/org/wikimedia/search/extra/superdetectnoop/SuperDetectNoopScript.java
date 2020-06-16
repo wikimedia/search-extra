612fbf1610322656a23a38ec30ae8851548a39b4
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-06-16 15:19:42 -0700

    Clarify variables in SuperDetectNoopScript::update
    This update function is complex and hard to read. Part of that
    complexity is the uncertainty of naming. Is `sourceValue` from the
    source, or the updateSource?
    
    This is separated from the followup patch that refactors this method
    as these old names made even reviewing the following refactor
    difficult as it wasn't clear if the correct variables were being
    referenced.
    
    * prefix variables as either old or new, depending on their source. This
      mimics how ChangeHandler names these variables as we pass them down.
    * Pull newEntry.getKey() into a variable named simply `key`, as it is
      shared between new and old.
    * always suffix Object variables with Raw and remove the Raw suffix
      when casting to a more concrete type.
    
    Change-Id: Ia2b4bd3ad8af0b9af09a11802f020324e651183c

--- extra/src/main/java/org/wikimedia/search/extra/superdetectnoop/SuperDetectNoopScript.java
+++ extra/src/main/java/org/wikimedia/search/extra/superdetectnoop/SuperDetectNoopScript.java
@@ -94,24 +94,25 @@ public class SuperDetectNoopScript extends UpdateScript {
     /**
      * Update old with the source and detector configuration of this script.
      */
-    UpdateStatus update(Map<String, Object> source, Map<String, Object> updateSource, String path) {
+    UpdateStatus update(Map<String, Object> oldSource, Map<String, Object> newSource, String path) {
         UpdateStatus modified = UpdateStatus.NOT_UPDATED;
-        for (Map.Entry<String, Object> sourceEntry : updateSource.entrySet()) {
-            String nextPath = path + sourceEntry.getKey();
-            Object sourceValue = source.get(sourceEntry.getKey());
-            ChangeHandler<Object> handler = pathToHandler.get(nextPath);
+        for (Map.Entry<String, Object> newEntry : newSource.entrySet()) {
+            String key = newEntry.getKey();
+            String entryPath = path + key;
+            Object oldValueRaw = oldSource.get(key);
+            ChangeHandler<Object> handler = pathToHandler.get(entryPath);
             if (handler == null) {
-                if (sourceEntry.getValue() instanceof Map) {
+                if (newEntry.getValue() instanceof Map) {
                     @SuppressWarnings("unchecked")
-                    Map<String, Object> nextSource = (Map<String, Object>) sourceValue;
-                    if (nextSource == null) {
-                        nextSource = new LinkedHashMap<>();
-                        source.put(sourceEntry.getKey(), nextSource);
+                    Map<String, Object> oldValue = (Map<String, Object>) oldValueRaw;
+                    if (oldValue == null) {
+                        oldValue = new LinkedHashMap<>();
+                        oldSource.put(key, oldValue);
                     }
                     // recursive merge maps
                     @SuppressWarnings("unchecked")
-                    Map<String, Object> nextUpdateSource = (Map<String, Object>) sourceEntry.getValue();
-                    UpdateStatus nextModified = update(nextSource, nextUpdateSource, nextPath + ".");
+                    Map<String, Object> newValue = (Map<String, Object>) newEntry.getValue();
+                    UpdateStatus nextModified = update(oldValue, newValue, entryPath + ".");
                     if (nextModified == UpdateStatus.NOOP_DOCUMENT) {
                         return nextModified;
                     } else if (nextModified == UpdateStatus.UPDATED) {
@@ -121,7 +122,7 @@ public class SuperDetectNoopScript extends UpdateScript {
                 }
                 handler = ChangeHandler.Equal.INSTANCE;
             }
-            ChangeHandler.Result result = handler.handle(sourceValue, sourceEntry.getValue());
+            ChangeHandler.Result result = handler.handle(oldValueRaw, newEntry.getValue());
             if (result.isDocumentNooped()) {
                 return UpdateStatus.NOOP_DOCUMENT;
             }
@@ -129,9 +130,9 @@ public class SuperDetectNoopScript extends UpdateScript {
                 continue;
             }
             if (result.newValue() == null) {
-                source.remove(sourceEntry.getKey());
+                oldSource.remove(key);
             } else {
-                source.put(sourceEntry.getKey(), result.newValue());
+                oldSource.put(key, result.newValue());
             }
             modified = UpdateStatus.UPDATED;
         }
