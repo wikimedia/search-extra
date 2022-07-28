8230a3713341a24b093306fcfa6a0d5dfb246cf5
Erik Bernhardson <ebernhardson@wikimedia.org>
2022-07-28 13:18:46 -0700

    multilist: Change internals to use Set instead of List
    Using lists as the internal representation has terrible behaviour for
    large n, particularly in MultiList.equalsIgnoreOrder method. Avoid the
    O(n^2) complexity of this method be replacing the Lists with Sets and
    using Set equality, which gives an O(n log n) complexity. The given
    test case with 100k elements takes more than 30s to complete on my
    laptop, while the replacement runs in ~100ms.
    
    Change-Id: Ia07c4beb622d1cd23c65198e07fed5d1637402f0

--- extra/src/main/java/org/wikimedia/search/extra/superdetectnoop/MultiListHandler.java
+++ extra/src/main/java/org/wikimedia/search/extra/superdetectnoop/MultiListHandler.java
@@ -1,17 +1,24 @@
 package org.wikimedia.search.extra.superdetectnoop;
 
+import static java.lang.Boolean.FALSE;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 
 /**
  * Implementation of {@link ChangeHandler} that allows for maintaining multiple
- * lists inside a single list stored within Elasticsearch. The sub lists are
- * updated in their entirety, while unreferenced lists in the source are maintained.
- * Lists can be removed by providing a single tombstone value, __DELETE_GROUPING__.
+ * sets inside a single list stored within Elasticsearch. The sub sets are
+ * updated in their entirety, while unreferenced sets in the source are maintained.
+ * Sets can be removed by providing a single tombstone value, __DELETE_GROUPING__.
  */
 public class MultiListHandler implements ChangeHandler.NonnullChangeHandler<List<String>> {
     static final String DELETE = "__DELETE_GROUPING__";
@@ -26,8 +33,8 @@ public class MultiListHandler implements ChangeHandler.NonnullChangeHandler<List
         if (newValue.isEmpty()) {
             throw new IllegalArgumentException("Empty update provided to MultiListHandler");
         }
-        MultiList original = MultiList.parse(oldValue);
-        MultiList update = MultiList.parse(newValue);
+        MultiSet original = MultiSet.parse(oldValue);
+        MultiSet update = MultiSet.parse(newValue);
         if (original.replaceFrom(update)) {
             return new ChangeHandler.Changed(original.flatten());
         } else {
@@ -35,52 +42,65 @@ public class MultiListHandler implements ChangeHandler.NonnullChangeHandler<List
         }
     }
 
-    private static final class MultiList {
+    private static final class MultiSet {
         private static final char DELIMITER = '/';
         private static final String UNNAMED = "__UNNAMED_GROUPING__";
-        private final Map<String, List<String>> lists;
+        private final Map<String, Set<String>> sets;
 
-        private MultiList(Map<String, List<String>> lists) {
-            this.lists = lists;
+        private MultiSet(Map<String, Set<String>> sets) {
+            this.sets = sets;
         }
 
-        static MultiList parse(Collection<String> strings) {
-            return new MultiList(strings.stream()
-                    .collect(Collectors.groupingBy(val -> {
-                        int pos = val.indexOf(DELIMITER);
-                        return pos == -1 ? UNNAMED : val.substring(0, pos);
-                    })));
+        static MultiSet parse(Collection<String> strings) {
+            return new MultiSet(strings.stream()
+                    .collect(groupingBy(val -> {
+                            int pos = val.indexOf(DELIMITER);
+                            return pos == -1 ? UNNAMED : val.substring(0, pos);
+                        }, toSet())));
         }
 
-        private boolean isDeleteMarker(String group, List<String> list) {
-            if (list.size() != 1) {
-                return false;
+        private <T> Optional<T> onlyElement(Collection<T> foo) {
+            Iterator<T> it = foo.iterator();
+            if (!it.hasNext()) {
+                return Optional.empty();
             }
-            String value = list.get(0);
-            // We don't need to verify the group, by construction the prefix must match
-            // the group. We only need to verify that there isn't additional content. Verify
-            // by ensuring there is only enough room for the marker and the prefix.
-            int expectedLength = DELETE.length();
-            if (!UNNAMED.equals(group)) {
-                expectedLength += 1 + group.length();
+            T value = it.next();
+            if (it.hasNext()) {
+                return Optional.empty();
+            } else {
+                return Optional.of(value);
             }
-            return value.length() == expectedLength && value.endsWith(DELETE);
         }
 
-        boolean replaceFrom(MultiList other) {
+        private boolean isDeleteMarker(String group, Set<String> values) {
+            return onlyElement(values)
+                .map(value -> {
+                    // We don't need to verify the group, by construction the prefix must match
+                    // the group. We only need to verify that there isn't additional content. Verify
+                    // by ensuring there is only enough room for the marker and the prefix.
+                    int expectedLength = DELETE.length();
+                    if (!UNNAMED.equals(group)) {
+                        expectedLength += 1 + group.length();
+                    }
+                    return value.length() == expectedLength && value.endsWith(DELETE);
+                })
+                .orElse(FALSE);
+        }
+
+        boolean replaceFrom(MultiSet other) {
             boolean changed = false;
-            for (Map.Entry<String, List<String>> entry : other.lists.entrySet()) {
-                List<String> current = lists.get(entry.getKey());
-                List<String> updated = entry.getValue();
+            for (Map.Entry<String, Set<String>> entry : other.sets.entrySet()) {
+                Set<String> current = sets.get(entry.getKey());
+                Set<String> updated = entry.getValue();
                 // If we are already holding an equivalent value skip the update.
-                if (current != null && equalsIgnoreOrder(current, updated)) {
+                if (current != null && current.equals(updated)) {
                     continue;
                 }
                 if (!isDeleteMarker(entry.getKey(), updated)) {
                     // Standard update
                     changed = true;
-                    lists.put(entry.getKey(), updated);
-                } else if (lists.remove(entry.getKey()) != null) {
+                    sets.put(entry.getKey(), updated);
+                } else if (sets.remove(entry.getKey()) != null) {
                     changed = true;
                 }
             }
@@ -88,14 +108,9 @@ public class MultiListHandler implements ChangeHandler.NonnullChangeHandler<List
         }
 
         List<String> flatten() {
-            return lists.entrySet().stream()
+            return sets.entrySet().stream()
                 .flatMap(entry -> entry.getValue().stream())
-                .collect(Collectors.toList());
-        }
-
-        private static <T> boolean equalsIgnoreOrder(Collection<T> a, Collection<T> b) {
-            // Expecting n <= 10, a fancier comparison doesn't seem justified
-            return a.size() == b.size() && a.containsAll(b);
+                .collect(toList());
         }
     }
 }
